37d1dda0f2dc917e6c5be1644b0169b2eabd23f1
sm <sebastian.matkovich@gmail.com>
2020-10-28 18:40:06 +0100

    successfully applied distance point to mesh and check if point is inside mesh

--- spherepack.py
+++ spherepack.py
@@ -2,4 +2,5 @@ import numpy as np
 import trimesh
 
 mesh = trimesh.load_mesh('btr.stl')
-trimesh.proximity.closest_point(mesh, [[0.0, 0.0, 0.0]])
+print(trimesh.proximity.closest_point(mesh, [[0.0, 0.0, 0.0]]))
+print(trimesh.proximity.signed_distance(mesh, [[0.0, 0.0, 0.0]]))
