98ce591546ee1574fa8de1d6fb7d62163d9420d5
sm <sebastian.matkovich@gmail.com>
2020-10-28 18:21:39 +0100

    changed to trimesh

--- spherepack.py
+++ spherepack.py
@@ -1,12 +1,4 @@
 import numpy
-from stl import mesh
+import trimesh
 
-# Using an existing stl file:
-your_mesh = mesh.Mesh.from_file('btr.stl')
-
-# The mesh normals (calculated automatically)
-print('normals')
-print(your_mesh.normals)
-# The mesh vectors
-print('vectors')
-print(your_mesh.v0, your_mesh.v1, your_mesh.v2)
+mesh = trimesh.load_mesh('btr.stl')
