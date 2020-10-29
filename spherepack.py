e7a3efac9e124d30b5e212f03ab3c06d32029bc1
sm <sebastian.matkovich@gmail.com>
2020-10-29 16:10:33 +0100

    first point in mesh

--- spherepack.py
+++ spherepack.py
@@ -4,3 +4,6 @@ import trimesh
 mesh = trimesh.load_mesh('btr.stl')
 print(trimesh.proximity.closest_point(mesh, [[0.0, 0.0, 0.0]]))
 print(trimesh.proximity.signed_distance(mesh, [[0.0, 0.0, 0.0]]))
+print(mesh.faces[13637])
+print(trimesh.proximity.closest_point(mesh, [[-1587.11165215, 2258.88125697, 2753.24604090]]))
+print(trimesh.proximity.signed_distance(mesh, [[-1587.11165215, 2258.88125697, 2753.24604090]]))
